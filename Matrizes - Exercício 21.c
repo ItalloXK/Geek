de3e1110fb5e2f629500581da748951d23096d74
ItalloXK <136774073+ItalloXK@users.noreply.github.com>
2023-09-16 17:13:25 -0300

    Matrizes - Exercício 21.c

--- Matrizes - Exercício 21.c
+++ Matrizes - Exercício 21.c
@@ -43,58 +43,61 @@ printf("Os valores da segunda matriz sao: \n\n");
   }
   
   //EXIBINDO O MENU
+  do{
+  printf("\nESCOLHA SOMENTE OPCOES DO MENU!\n\n");
   printf("Digite 1 para somar as matrizes!\n\nDigite 2 para subtrair a primeira matriz da segunda matriz!\n\nDigite 3 para adicionar uma constante para as duas matrizes!\n\n");
   scanf("%d", &escolha);
   
-  switch(escolha){
-    case 1:
-      printf("O resultado da soma eh: \n\n");
-      for(i=0; i<2; i++){
-        for(j=0; j<2; j++){
-         matrizSoma[i][j] = matrizA[i][j] + matrizB[i][j];
-        }
-      }
-      for(i=0; i<2; i++){
-        for(j=0; j<2; j++){
-          printf("%.2f  ", matrizSoma[i][j]);
-        }printf("\n\n");
-      }
-      break;
-    case 2:
-      printf("O resultado da subtracao eh: \n\n");
-      for(i=0; i<2; i++){
-        for(j=0; j<2; j++){
-         matrizSubtracao[i][j] = matrizB[i][j] - matrizA[i][j];
-        }
-      }
-      for(i=0; i<2; i++){
-        for(j=0; j<2; j++){
-          printf("%.2f  ", matrizSubtracao[i][j]);
-        }printf("\n\n");
-      }
-	break;
-    case 3:
-      printf("Digite a constante: ");
-      scanf("%d", &constante);
-      printf("\nA primeira matriz com a constante adicionada eh: \n\n");
-      for(i=0; i<2; i++){
-        for(j=0; j<2; j++){
-          matrizA[i][j] = matrizA[i][j] + constante;
-          matrizB[i][j] = matrizB[i][j] + constante;
-        }
-      }
-      for(i=0; i<2; i++){
-        for(j=0; j<2; j++){
-          printf("%.2f  ", matrizA[i][j]);
-        }printf("\n\n");
-      }
-      printf("\nA segunda matriz com a constante adicionada eh: \n\n");
-      for(i=0; i<2; i++){
-        for(j=0; j<2; j++){
-          printf("%.2f  ", matrizB[i][j]);
-        }printf("\n\n");
-      }
-      break;
-  }
+	  switch(escolha){
+		case 1:
+	      printf("O resultado da soma eh: \n\n");
+	      for(i=0; i<2; i++){
+	        for(j=0; j<2; j++){
+	         matrizSoma[i][j] = matrizA[i][j] + matrizB[i][j];
+	        }
+	      }
+	      for(i=0; i<2; i++){
+	        for(j=0; j<2; j++){
+	          printf("%.2f  ", matrizSoma[i][j]);
+	        }printf("\n\n");
+	      }
+	      break;
+	    case 2:
+	      printf("O resultado da subtracao eh: \n\n");
+	      for(i=0; i<2; i++){
+	        for(j=0; j<2; j++){
+	         matrizSubtracao[i][j] = matrizB[i][j] - matrizA[i][j];
+	        }
+	      }
+	      for(i=0; i<2; i++){
+	        for(j=0; j<2; j++){
+	          printf("%.2f  ", matrizSubtracao[i][j]);
+	        }printf("\n\n");
+	      }
+			break;
+	    case 3:
+	      printf("Digite a constante: ");
+	      scanf("%d", &constante);
+	      printf("\nA primeira matriz com a constante adicionada eh: \n\n");
+	      for(i=0; i<2; i++){
+	        for(j=0; j<2; j++){
+	          matrizA[i][j] = matrizA[i][j] + constante;
+	          matrizB[i][j] = matrizB[i][j] + constante;
+	        }
+	      }
+	      for(i=0; i<2; i++){
+	        for(j=0; j<2; j++){
+	          printf("%.2f  ", matrizA[i][j]);
+	        }printf("\n\n");
+	      }
+	      printf("\nA segunda matriz com a constante adicionada eh: \n\n");
+	      for(i=0; i<2; i++){
+	        for(j=0; j<2; j++){
+	          printf("%.2f  ", matrizB[i][j]);
+	        }printf("\n\n");
+	      }
+	    	break;
+	  }
+  }while(escolha < 1 || escolha > 3);
   return 0;
 }
